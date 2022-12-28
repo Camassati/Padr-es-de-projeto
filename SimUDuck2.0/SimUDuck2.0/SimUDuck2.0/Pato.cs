0d32e3c83039074718fd5350c279299f921b0197
Gustavo Martins <gfirminocruz@gmail.com>
2022-12-28 20:23:10 -0300

    SimUDuck2.0

---
+++ SimUDuck2.0/SimUDuck2.0/SimUDuck2.0/Pato.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck2._0
+{
+    public abstract class Pato
+    {
+        internal void VoarNaoSei()
+        {
+            Console.WriteLine("Voar: Não sei voar");
+        }
+        internal void VoarSei()
+        {
+            Console.WriteLine("Voar: To voando");
+        }
+        internal void Boiando()
+        {
+            Console.WriteLine("Boiar: Todos boiam\n");
+        }
+        internal void QuackSei()
+        {
+            Console.WriteLine("Som: Quack");
+        }
+        internal void QuackMudo()
+        {
+            Console.WriteLine("Som: --Silencio--");
+        }
+        internal void QuackRangido()
+        {
+            Console.WriteLine("Som: Rangido");
+        }
+        internal abstract void Display();
+    }
+
+}
