97fca318e2d7080fc9af1b9830612f424e3d35a1
Gustavo Martins <gfirminocruz@gmail.com>
2024-01-08 17:25:41 -0300

    AdapterPattern

---
+++ Adapter/Adapter.C#/AdapterPattern/AdapterPattern/Model/LumiaMobile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern.Model
+{
+    public class LumiaMobile
+    {
+        public string IdModelo { get; set; }
+        public string Altura { get; set; }
+        public string Largura { get; set; }
+        public string Espessura { get; set; }
+
+        public string Peso { get; set; }
+    }
+}
---
+++ Adapter/Adapter.C#/AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdapterPattern;
+using AdapterPattern.Interfaces;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ILumiaXMLTarget lumiaXMLTarget = new LumiaXMLAdapter();
+        var xmlLumiaMobile = lumiaXMLTarget.GetLumiaMobilesXMLSpecifications();
+        Console.WriteLine(xmlLumiaMobile.InnerText);
+    }
+}
