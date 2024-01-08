2675679731bc56c45f2c256f0737e187d00c76ac
Gustavo Martins <gfirminocruz@gmail.com>
2024-01-08 11:27:28 -0300

    Singleton safe trhead

---
+++ Factory/Factory.C#/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using SingletonPattern;
+using System;
+
+
+
+Thread t1 = new Thread(() =>
+{
+    var instace = UploadService.Instance(1);
+});
+
+Thread t2 = new Thread(() =>
+{
+    var instace = UploadService.Instance(2);
+});
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
