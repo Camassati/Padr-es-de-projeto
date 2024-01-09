efe049f8c3d9b8a698238382def19cc0959544af
Gustavo Martins <gfirminocruz@gmail.com>
2024-01-09 09:55:39 -0300

    StatePattern

---
+++ State/StatePatternC#/StatePatternC#/Program.cs
@@ -0,0 +1,16 @@
+﻿using StatePatternC_;
+
+var car = new Car();
+Console.WriteLine($"State: {car.CurrntState}\n");
+
+car.TakeAction(Car.Action.Start);
+Console.WriteLine($"State: {car.CurrntState}\n");
+
+car.TakeAction(Car.Action.Start);
+Console.WriteLine($"State: {car.CurrntState} \n");
+
+car.TakeAction(Car.Action.Accelerate);
+Console.WriteLine($"State: {car.CurrntState} \n");
+
+car.TakeAction(Car.Action.Stop);
+Console.WriteLine($"State: {car.CurrntState} \n");
